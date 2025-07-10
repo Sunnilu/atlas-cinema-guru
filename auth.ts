ce522547220b893f5f52a115f3dfb619529d2749
Suntha <Suntha.Lucas@atlasschool.com>
2025-07-10 11:55:16 -0500

    next auth

--- auth.ts
+++ auth.ts
@@ -1,24 +1,29 @@
 // auth.ts
 
 import NextAuth from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
+import GitHub from "next-auth/providers/github";
+import type { NextAuthConfig } from "next-auth";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+const config: NextAuthConfig = {
   theme: {
     brandColor: "#1ED2AF",
     logo: "/logo.png",
     buttonText: "#ffffff",
   },
   providers: [
-    GitHubProvider({
+    GitHub({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
+  pages: {
+    signIn: "/login", // optional custom login page
+  },
   callbacks: {
-    authorized: async ({ auth }) => {
-      // Logged in users are authenticated
-      return !!auth;
+    authorized({ auth }) {
+      return !!auth?.user; // only allow if user is authenticated
     },
   },
-});
+};
+
+export const { handlers, signIn, signOut, auth } = NextAuth(config);
