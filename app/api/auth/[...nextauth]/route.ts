eae82ad18f00984b8560d8abca54baf2f45fce7c
Suntha <Suntha.Lucas@atlasschool.com>
2025-07-10 11:27:57 -0500

    edge

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,4 +4,4 @@ import { handlers } from "@/auth"; // Refers to your configured auth.ts
 
 export const { GET, POST } = handlers;
 
-export const runtime = "edge"; // Optional, enables edge functions for faster auth
+
