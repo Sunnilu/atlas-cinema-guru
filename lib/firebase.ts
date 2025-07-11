e8840ca5e46774b1319d82c5c95fe4f6474763da
Suntha <Suntha.Lucas@atlasschool.com>
2025-07-11 15:50:48 -0500

    adding firebase

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,16 +2,19 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// Your config from Firebase Console
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCAMBYLQV4UszJBFpzWNfMXJ2Ax3t1cbpE",
+  authDomain: "atlas-cinema-guru.firebaseapp.com",
+  projectId: "atlas-cinema-guru",
+  storageBucket: "atlas-cinema-guru.appspot.com",
+  messagingSenderId: "704533741978",
+  appId: "1:704533741978:web:1def992dbd6d2599543a23",
+  measurementId: "G-LWQJ4ZL9WK",
 };
 
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
-const db = getFirestore(app);
+// Prevent reinitializing on hot reloads in dev
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-export { db };
+// Export Firestore
+export const db = getFirestore(app);
