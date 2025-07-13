37ab56837438f9b52071cf7c43b213a4441a72f1
Suntha <Suntha.Lucas@atlasschool.com>
2025-07-13 18:17:31 -0500

    added types

--- lib/types/next-auth.d.ts
+++ lib/types/next-auth.d.ts
@@ -1,16 +1,14 @@
 // types/next-auth.d.ts
-
-import NextAuth, { DefaultSession } from "next-auth"
-import { JWT } from "next-auth/jwt"
+import NextAuth from "next-auth";
 
 declare module "next-auth" {
-  interface Session extends DefaultSession {
-    accessToken?: string
+  interface Session {
+    accessToken?: string;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
-    accessToken?: string
+    accessToken?: string;
   }
 }
