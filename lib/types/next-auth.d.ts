dd2c58813e21db0972b54c57efef4e386c79e22c
Suntha <Suntha.Lucas@atlasschool.com>
2025-07-13 18:13:25 -0500

    to handle session access Token

---
+++ lib/types/next-auth.d.ts
@@ -0,0 +1,16 @@
+// types/next-auth.d.ts
+
+import NextAuth, { DefaultSession } from "next-auth"
+import { JWT } from "next-auth/jwt"
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    accessToken?: string
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    accessToken?: string
+  }
+}
