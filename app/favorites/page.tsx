40f017cc195a3bc5839ac0a318c0adf14614c831
Suntha <Suntha.Lucas@atlasschool.com>
2025-07-14 07:40:42 -0500

    favorites

---
+++ app/favorites/page.tsx
@@ -0,0 +1,8 @@
+export default function FavoritesPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Your Favorites</h1>
+      <p>Coming soon...</p>
+    </div>
+  );
+}
